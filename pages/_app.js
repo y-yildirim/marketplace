cb332c20b7aadcdc393f1c197d489f657cf28dd3
y-yildirim <y.yildirim.ce@gmail.com>
2023-02-16 15:37:22 +0300

    add logo and theme checkbox to navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
+
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <Navbar />
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Component {...pageProps} />
+    </div>
+    <Script src="https://kit.fontawesome.com/054fc2b270.js" crossorigin="anonymous" />
+    <Footer />
+  </ThemeProvider>
+);
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Home = () => (
   <div>
     <h1 className="text-3xl font-bold underline">
@@ -6,4 +8,4 @@ const Home = () => (
   </div>
 );
 
-export default Home();
+export default Home;
